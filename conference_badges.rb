a209a85d254802e14f41c6879f79585aed0d0121
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-17 21:51:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+	x = []
+ 	for i in array do
+		x << badge_maker(i)
+	end
+	return x
+end
+
+def assign_rooms(array)
+	x=1
+	y = []
+	for i in array do
+		y <<  “Hello, #{i}! You'll be assigned to room #{x}!”
+		x+=1	
+	end
+	return x
+end
+
+def printer(array)
+	batch_badge_creator(array)
+	assign_rooms(array)
+end
